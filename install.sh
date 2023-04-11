67dc6192076bb5453ac9f2daf2d0db9615f8f6ef
choi jinwon <97656142+randomwons@users.noreply.github.com>
2023-04-12 02:29:35 +0900

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+# Update and upgrade the system
+sudo apt-get update
+sudo apt-get -y upgrade
+
+# Install required packages
+sudo apt-get -y install \ 
+  build-essential \ 
+  flex \ 
+  bison \
+  libssl-dev \
+  libelf-dev \
+  libncurses-dev \
+  autoconf \
+  libudev-dev \
+  libtool \
+  dwarves \
+  bc \
+  wslu \
+  hwdata
+
+# Get kernel version checkout the corresponding branch
+VERSION=$(uname -r | cut -d '-' -f1)
+git clone https://github.com/microsoft/WSL2-Linux-Kernel.git
+cd WSL2-Linux-Kernel
+git checkout linux-msft-wsl-${VERSION}
+
+wget https://github.com/randomwons/wsl2-camera/blob/main/config
+sudo mv config .config
+
+
+# Build and install kernel
+sudo make -j$(nproc) \
+  && sudo make modules_install -j$(nproc) \
+  && sudo make install -j$(nproc)
+
+# Install USBIP
+cd tools/usb/usbip
+sudo ./autogen.sh
+sudo ./configure
+sudo make install -j$(nproc)
+
+# Copy libusbip.so.0 to /lib
+sudo cp libsrc/.libs/libusbip.so.0 /lib/libusbip.so.0
+
+# Copy kerner image to Windows
+HOST_USERNAME=$(wslpath "$(wslvar USERPROFILE)" | cut -d '/' -f5)
+sudo cp arch/x86/boot/bzImage /mnt/c/Users/${HOST_USERNAME}/usbip-bzImage
+
+echo "[wsl2]" >> /mnt/c/Users/${HOST_USERNAME}/.wslconfig
+echo "kernel=C:\\Users\\${HOST_USERNAME}\\usbip-bzImage" >> /mnt/c/Users/${HOST_USERNAME}/.wslconfig
