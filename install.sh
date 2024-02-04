92e463e3318afdf268dce83bb07a5b80bd4c9dd2
choi jinwon <97656142+randomwons@users.noreply.github.com>
2024-02-05 00:59:04 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Update and upgrade the system
-sudo apt-get update
+sudo apt-get -y update
 sudo apt-get -y upgrade
 
 # Install required packages
@@ -9,13 +9,23 @@ sudo apt-get -y install build-essential flex bison libssl-dev libelf-dev libncur
 
 # Get kernel version checkout the corresponding branch
 VERSION=$(uname -r | cut -d '-' -f1)
-git clone https://github.com/microsoft/WSL2-Linux-Kernel.git
+git clone -b linux-msft-wsl-${VERSION} https://github.com/microsoft/WSL2-Linux-Kernel.git
 cd WSL2-Linux-Kernel
-git checkout linux-msft-wsl-${VERSION}
 
 # config
-sudo mv ../config .config
+sudo cp /proc/config.gz config.gz
+sudo gunzip config.gz
+sudo mv config .config
 
+sudo scripts/config --enable CONFIG_MEDIA_SUPPORT
+sudo scripts/config --enable CONFIG_MEDIA_SUPPORT_FILTER
+sudo scripts/config --enable CONFIG_MEDIA_SUBDRV_AUTOSELECT
+sudo scripts/config --enable CONFIG_MEDIA_CAMERA_SUPPORT
+sudo scripts/config --enable CONFIG_VIDEO_V4L2_SUBDEV_API
+sudo scripts/config --enable CONFIG_MEDIA_USB_SUPPORT
+sudo scripts/config --enable CONFIG_USB_VIDEO_CLASS
+sudo scripts/config --enable CONFIG_USB_VIDEO_CLASS_INPUT_EVDEV
+sudo scripts/config --enable CONFIG_USB_GSPCA
 
 # Build and install kernel
 sudo make -j$(nproc) && sudo make modules_install -j$(nproc) && sudo make install -j$(nproc)
